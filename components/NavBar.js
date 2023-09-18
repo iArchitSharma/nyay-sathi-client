8cbfe80689b73139817840a38ebf479c89489883
Abhishek Gobind Rao <abhishek.rao.4200@gmail.com>
2023-09-18 18:12:08 +0530

    xyz

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,9 +1,11 @@
 // components/NavBar.js
 
-import Link from 'next/link';
-import { FiBell, FiMessageCircle, FiUser } from 'react-icons/fi'; // Import notification, message, and profile icons
+import Link from "next/link";
+import { FiBell, FiMessageCircle, FiUser } from "react-icons/fi"; // Import notification, message, and profile icons
 
-const NavBar = () => {
+const NavBar = ({ user }) => {
+  const isAdvocate = user && user.type === "Advocate";
+
   return (
     <header className="bg-white shadow-lg">
       <div className="container mx-auto px-6 py-3">
@@ -12,42 +14,51 @@ const NavBar = () => {
           <div className="flex items-center">
             {/* Logo */}
             <Link href="/">
-              <div className="text-xl font-semibold text-gray-800 mr-4">Your Logo</div>
+              <div className="text-xl font-semibold text-gray-800 mr-4">
+                Your Logo
+              </div>
             </Link>
             {/* Navigation Links */}
-            <nav className="space-x-4 flex items-center"> {/* Use flex to align items horizontally */}
-              <Link href="/users">
-                <div className="text-gray-600 hover:text-gray-800 transition duration-300">Find Advocate</div>
+            <nav className="space-x-4 flex items-center">
+              <Link href={isAdvocate ? "/projects" : "/users"}>
+                <div className="text-gray-600 hover:text-gray-800 transition duration-300">
+                  {isAdvocate ? "Find a Project" : "Find Advocate"}
+                </div>
               </Link>
               {/* Manage Link */}
               <Link href="/manage">
-                <div className="text-gray-600 hover:text-gray-800 transition duration-300 ml-4">Manage</div> {/* Add ml-4 for spacing */}
+                <div className="text-gray-600 hover:text-gray-800 transition duration-300 ml-4">
+                  Manage
+                </div>{" "}
+                {/* Add ml-4 for spacing */}
               </Link>
             </nav>
           </div>
-          
+
           {/* Right Side */}
           <div className="flex items-center space-x-4 ml-auto">
             {/* Notification Icon */}
             <div className="relative group">
               <FiBell className="text-gray-600 group-hover:text-gray-800 text-xl cursor-pointer transition duration-300" />
-              <span className="bg-red-500 text-white text-xs font-semibold rounded-full w-4 h-4 absolute top-0 right-0 -mt-1 -mr-1">3</span>
+              <span className="bg-red-500 text-white text-xs font-semibold rounded-full w-4 h-4 absolute top-0 right-0 -mt-1 -mr-1">
+                3
+              </span>
             </div>
-            
+
             {/* Message Icon */}
             <Link href="/messages">
               <div>
                 <FiMessageCircle className="text-gray-600 hover:text-gray-800 text-xl cursor-pointer transition duration-300" />
               </div>
             </Link>
-            
+
             {/* Post a Legal Project Button */}
             <Link href="/post">
               <div className="bg-blue-400 hover:bg-blue-500 text-white py-2 px-4 rounded-full text-sm font-semibold transition duration-300">
                 Post a Legal Project
               </div>
             </Link>
-            
+
             {/* Profile Icon */}
             <Link href="/profile">
               <div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
 import '@/styles/globals.css'
 import NavBar from '@/components/NavBar';
 
+const user = {
+  profileImage: "/test/profile.jpg",
+  fullName: "John Doe",
+  firstName: "John",
+  username: "johndoe",
+  location: "Gaya, Bihar",
+  experience: 5,
+  rating: 3,
+  bio: "Web Developer with a passion for coding.",
+  portfolioLink: "https://example.com/johndoe-portfolio",
+  type: "Advocate",
+  enrollment: "AP/00107/2020",
+};
+
 export default function App({ Component, pageProps }) {
   return (
     <div>
-      <NavBar/>
+      <NavBar user={user}/>
       <Component {...pageProps} />
     </div>
   );
